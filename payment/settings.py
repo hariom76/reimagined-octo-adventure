61876cd4bdf06c4c4124584e0d849c314c42329d
hariom76 <hariommandal24@gmail.com>
2020-11-14 16:25:04 +0530

    changesww

--- payment/settings.py
+++ payment/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '8g@b$ub+rsh)zb_1o#(0qz)xu5=f8$0%&2-0o$7+!#vfxl3!-='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['thesparks.herokuapp.com']
 
 
 # Application definition
