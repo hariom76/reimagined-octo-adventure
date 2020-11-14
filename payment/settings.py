265797930c37a4d9780f9e5e453ccd14999663cf
hariom76 <hariommandal24@gmail.com>
2020-11-14 23:55:32 +0530

    setting

--- payment/settings.py
+++ payment/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path,os
+import dj-database-url
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,10 +82,12 @@ DATABASES = {
         'NAME': 'hariom',
         'USER': 'postgres',
         'PASSWORD': '1234',
-        'HOST': '',
+        'HOST': 'localhost',
         'PORT' : '5432'
     }
 }
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 
