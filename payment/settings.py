4feb9d5fd63f959277b2ab23419d3a1361a9fd14
hariom76 <hariommandal24@gmail.com>
2021-07-01 23:15:22 +0530

    'dlskaal

--- payment/settings.py
+++ payment/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '8g@b$ub+rsh)zb_1o#(0qz)xu5=f8$0%&2-0o$7+!#vfxl3!-='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://twinkleloot.herokuapp.com']
+ALLOWED_HOSTS = 'https://twinkleloot.herokuapp.com'
 
 
 # Application definition
